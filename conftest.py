3ee61412312b861c733d670e2f951afe3908adf1
DESKTOP-GUJU61I\Антон <anton.chertkov@gmail.com>
2021-05-12 23:35:18 +0400

    update conftest

--- conftest.py
+++ conftest.py
@@ -20,39 +20,45 @@ def browser(request):
 
     if browser_name == "chrome":
         options = ChromeOptions()
-        if headless == "yes":
-            options.add_argument('--headless')  # безголовый запуск браузера
-        options.add_argument('--start-maximized')  # запуск браузера "во весь экран
-        # options.add_argument('--window-size=1920x1080') # запуск браузера с заданным разрешением
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
 
-        browser = webdriver.Chrome(options=options)
+        if headless == "yes":
+            options.add_argument('--headless')  # безголовый запуск браузера
+        options.add_argument('--start-maximized')  # запуск браузера "во весь экран"
+        # options.add_argument('--window-size=1920x1080') # запуск браузера с заданным разрешением
 
+        browser = webdriver.Chrome(options=options)
         # browser.maximize_window() # "во весь экран" после запуска браузера
         # browser.set_window_size(1920, 1080) # изменение разрешения после запуска браузера
 
 
     elif browser_name == "firefox":
         options = FirefoxOptions()
+        options.set_preference('intl.accept_languages', user_language)
+
         if headless == "yes":
             options.add_argument('--headless')
         options.add_argument('--start-maximized')
-        options.set_preference('intl.accept_languages', user_language)
 
         browser = webdriver.Firefox(options=options)
+        # browser.maximize_window()
+        # browser.set_window_size(1920, 1080)
 
 
     elif browser_name == "yandex":
         options = ChromeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         # options.add_argument('--headless')  # в таком виде не работает с яндекс:(
         options.add_argument('--start-maximized')
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
 
         binary_yandex_driver_file = r'C:\Python\Scripts\yandexdriver.exe'
         browser = webdriver.Chrome(binary_yandex_driver_file, options=options)
 
         new_window = browser.window_handles[1]  # переход на второю вкладку
         browser.switch_to.window(new_window)
+        # browser.maximize_window()
+        # browser.set_window_size(1920, 1080)
+
 
     else:
         raise pytest.UsageError("--browser should be chrome or firefox or yandex")
